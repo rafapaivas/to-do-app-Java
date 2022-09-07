642ed22249ab8a4c50f105a3eb7049748cd927cc
Rafael Paiva <rafa.ps09@gmail.com>
2022-09-07 18:41:46 -0300

    Projeto lista de tarefas feito no bootcamp

---
+++ src/main/java/todoapplication/Main.java
@@ -0,0 +1,15 @@
+package todoapplication;
+/**
+ *
+ * @author Rafael Paiva
+ */
+import java.sql.Connection;
+import util.ConnectionFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Connection c = ConnectionFactory.getConnection();
+        ConnectionFactory.closeConnection(c);
+    }
+    
+}
